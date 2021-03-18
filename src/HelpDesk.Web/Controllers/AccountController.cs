966cb43843ba0196cdf1d632cb09b6b184fa98e8
Aleksey <arhangelruh@gmail.com>
2021-03-18 14:40:22 +0300

    init: add methods Login, Logout, Change Password to controller, view and view models from this methods

--- src/HelpDesk.Web/Controllers/AccountController.cs
+++ src/HelpDesk.Web/Controllers/AccountController.cs
@@ -1,29 +1,134 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HelpDesk.DAL.Models;
+﻿using HelpDesk.DAL.Models;
+using HelpDesk.Web.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace HelpDesk.Web.Controllers
 {
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
         /// <summary>
-        /// Сonstructor
+        /// Сonstructor.
         /// </summary>
         /// <param name="userManager"></param>
 
-        public AccountController(UserManager<User> userManager)
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
         }
-        public IActionResult Index()
+
+        /// <summary>
+        /// Login model.
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns>Login view</returns>
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
         {
-            return View();
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        /// <summary>
+        /// Login.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>Login result</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return View(model);
+        }
+
+        /// <summary>
+        /// Logout.
+        /// </summary>
+        /// <returns>Home view</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>
+        /// Change password model.
+        /// </summary>
+        /// <returns>User model</returns>
+        public async Task<IActionResult> ChangePassword()
+        {
+            var username = HttpContext.User.Identity.Name;
+            User user = await _userManager.FindByNameAsync(username);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ChangePasswordViewModel model = new ChangePasswordViewModel { Id = user.Id, UserName = user.UserName };
+            return View(model);
+        }
+
+        /// <summary>
+        /// Change password.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>Change password result</returns>
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var username = HttpContext.User.Identity.Name;
+                User user = await _userManager.FindByNameAsync(username);
+
+                if (user != null)
+                {
+                    IdentityResult result =
+                        await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+            }
+            return View(model);
         }
     }
 }
