69371bc975350d4a18032c05cd1641a27c848869
Aleksey <arhangelruh@gmail.com>
2021-06-24 15:29:16 +0300

    init: get statuses, status view model, refactor css: correctly show header image

--- src/HelpDesk.Web/Controllers/RequestController.cs
+++ src/HelpDesk.Web/Controllers/RequestController.cs
@@ -1,11 +1,24 @@
-﻿using HelpDesk.Common.Constants;
+﻿using HelpDesk.BLL.Interfaces;
+using HelpDesk.Common.Constants;
+using HelpDesk.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HelpDesk.Web.Controllers
 {
     public class RequestController : Controller
     {
+        private readonly IRequestsService _requestsService;
+
+        public RequestController(IRequestsService requestsService)
+        {
+            _requestsService = requestsService ?? throw new ArgumentNullException(nameof(requestsService));
+        }
+
         /// <summary>
         /// View whith properties links
         /// </summary>
@@ -15,6 +28,36 @@ namespace HelpDesk.Web.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Get statuses
+        /// </summary>
+        /// <returns>List status model</returns>
+        [Authorize(Roles = UserConstants.AdminRole)]
+        [HttpGet]
+        public async Task<IActionResult> Statuses()
+        {
+            var statuses = await _requestsService.GetStatusesAsync();
+            var models = new List<StatusViewModel>();
 
+            if (statuses.Any())
+            {
+                foreach (var status in statuses)
+                {                    
+                    models.Add(
+                    new StatusViewModel
+                    {
+                        Id = status.Id,
+                        StatusName = status.StatusName,
+                        Queue = status.Queue,
+                        Access = status.Access
+                    });
+                }
+                return View(models);
+            }
+            else
+            {
+                return Content("Статусы не найдены");
+            }           
+        }
     }
 }
---
+++ src/HelpDesk.Web/ViewModels/StatusViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.Web.ViewModels
+{
+    /// <summary>
+    /// Status model.
+    /// </summary>
+    public class StatusViewModel
+    {
+        /// <summary>
+        /// Status id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Satatus name.
+        /// </summary>
+        [Required(ErrorMessage = "Наименование статуса не может быть пустым")]
+        public string StatusName { get; set; }
+
+        /// <summary>
+        /// Order number.
+        /// </summary>
+        [Required(ErrorMessage = "Номер в очереди не может быть пустым")]
+        public int Queue { get; set; }
+
+        /// <summary>
+        /// Access status from user.
+        /// </summary>
+        [Required(ErrorMessage = "Поле доступность не может быть пустым")]
+        public bool Access { get; set; }
+    }
+}
