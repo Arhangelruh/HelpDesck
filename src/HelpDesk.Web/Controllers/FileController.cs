8f1bfff0a5a59c8f4abe5427d5cf271b6a7943be
Aleksey <arhangelruh@gmail.com>
2023-01-31 14:00:25 +0300

    init: add fileHelper and FileController for danload file process

--- src/HelpDesk.Web/Controllers/FileController.cs
+++ src/HelpDesk.Web/Controllers/FileController.cs
@@ -1,10 +1,17 @@
 ﻿using HelpDesk.BLL.Interfaces;
 using HelpDesk.BLL.Models;
 using HelpDesk.Common.Constants;
+using HelpDesk.Web.Services;
 using HelpDesk.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -13,11 +20,26 @@ namespace HelpDesk.Web.Controllers
     public class FileController : Controller
     {
         private readonly IFileService _fileService;
+        private readonly IWebHostEnvironment _environment;
+        private readonly ILogger<FileController> _logger;
 
-        public FileController(IFileService fileService)
+        private readonly string _fileDirectory;
+
+        private static readonly FormOptions _defaultFormOptions = new FormOptions();
+
+        private readonly string[] _permittedExtensions = { ".doc", ".docx", ".zip", ".jpg", ".jpeg", ".png" };
+        private readonly string _targetInstructionPath;
+
+
+        public FileController(IFileService fileService, IWebHostEnvironment environment, ILogger<FileController> logger, IConfiguration config)
         {
             _fileService = fileService ?? throw new ArgumentNullException(nameof(fileService));
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _targetInstructionPath = config.GetValue<string>("StoredInstructionPath");
+            _fileDirectory = Path.Combine(this._environment.WebRootPath, _targetInstructionPath);
         }
+
         /// <summary>
         /// Save file.
         /// </summary>
@@ -28,6 +50,7 @@ namespace HelpDesk.Web.Controllers
         [DisableRequestSizeLimit,
     RequestFormLimits(MultipartBodyLengthLimit = int.MaxValue,
         ValueLengthLimit = int.MaxValue)]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddFile(SaveFileViewModel saveFile, int Id)
         {
             if (!ModelState.IsValid)
@@ -98,6 +121,73 @@ namespace HelpDesk.Web.Controllers
         {
             await _fileService.DeleteFileAsync(fileId);
             return RedirectToAction("GetRequest", "Request", new { requestId });
+        }
+
+
+        /// <summary>
+        /// Get files instructions from folder.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Instructions()
+        {
+            //var fileDirectory = Path.Combine(this._environment.WebRootPath,
+            //"instructions");
+
+            List<InstructionViewModel> fileModels = new();
+
+            foreach (var item in Directory.GetFiles(_fileDirectory))
+            {
+
+                fileModels.Add(new InstructionViewModel()
+                {
+                    FileName = Path.GetFileName(item),
+                    FilePath = "/" + _targetInstructionPath + $"/{Path.GetFileName(item)}"
+                });
+            }
+
+            return View(fileModels);
+        }
+
+        [HttpGet]
+        public IActionResult AddFilePhysical()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Save  file.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>Requests view</returns>
+        // [Authorize]
+        [HttpPost]
+      //  [ValidateAntiForgeryToken]
+        [DisableRequestSizeLimit, RequestFormLimits(MultipartBodyLengthLimit = UploadFileConstant.UploadMiddleValue)]
+        public async Task<IActionResult> AddFilePhysical(UploadInstructionViewModel instruction)
+        {
+            if (ModelState.IsValid)
+            {
+                var streamedFileContent = await FileHelpers.ChangeFormFile(instruction.FileBody,
+                     ModelState, _permittedExtensions, UploadFileConstant.UploadMiddleValue);
+            }
+
+
+            if (!ModelState.IsValid)
+            {                
+                return BadRequest(ModelState);
+            }
+
+            if (instruction.FileBody.Length > 0)
+            {
+
+
+                string fileName = Path.GetFileName(instruction.FileBody.FileName);
+
+                using var stream = System.IO.File.Create(_fileDirectory + "/" + fileName);
+                await instruction.FileBody.CopyToAsync(stream);
+            }
+
+            return RedirectToAction("Instructions");
         }
     }
 }
---
+++ src/HelpDesk.Web/Services/FileHelpers.cs
@@ -0,0 +1,266 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Web.Services
+{
+    public class FileHelpers
+    {
+        private static readonly byte[] _allowedChars = Array.Empty<byte>();
+
+        private static readonly Dictionary<string, List<byte[]>> _fileSignature = new()
+        {
+            { ".gif", new List<byte[]> { new byte[] { 0x47, 0x49, 0x46, 0x38 } } },
+            { ".png", new List<byte[]> { new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A } } },
+            { ".jpeg", new List<byte[]>
+                {
+                    new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
+                    new byte[] { 0xFF, 0xD8, 0xFF, 0xE2 },
+                    new byte[] { 0xFF, 0xD8, 0xFF, 0xE3 },
+                }
+            },
+            { ".jpg", new List<byte[]>
+                {
+                    new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
+                    new byte[] { 0xFF, 0xD8, 0xFF, 0xE1 },
+                    new byte[] { 0xFF, 0xD8, 0xFF, 0xE8 },
+                }
+            },
+            { ".zip", new List<byte[]>
+                {
+                    new byte[] { 0x50, 0x4B, 0x03, 0x04 },
+                    new byte[] { 0x50, 0x4B, 0x4C, 0x49, 0x54, 0x45 },
+                    new byte[] { 0x50, 0x4B, 0x53, 0x70, 0x58 },
+                    new byte[] { 0x50, 0x4B, 0x05, 0x06 },
+                    new byte[] { 0x50, 0x4B, 0x07, 0x08 },
+                    new byte[] { 0x57, 0x69, 0x6E, 0x5A, 0x69, 0x70 },
+                }
+            },
+            { ".docx", new List<byte[]>
+                {
+                    new byte[] { 0x50, 0x4B, 0x03, 0x04 },
+                    new byte[] { 0x50, 0x4B, 0x4C, 0x49, 0x54, 0x45 },
+                    new byte[] { 0x50, 0x4B, 0x53, 0x70, 0x58 },
+                    new byte[] { 0x50, 0x4B, 0x05, 0x06 },
+                    new byte[] { 0x50, 0x4B, 0x07, 0x08 },
+                    new byte[] { 0x57, 0x69, 0x6E, 0x5A, 0x69, 0x70 },
+                }
+            },
+            { ".doc", new List<byte[]>
+                {  new byte[] { 0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1 }}
+            }
+        };
+
+        public static async Task<byte[]> ProcessFormFile<T>(IFormFile formFile,
+            ModelStateDictionary modelState, string[] permittedExtensions,
+            long sizeLimit)
+
+        {
+            var fieldDisplayName = string.Empty;
+
+            MemberInfo property =
+                typeof(T).GetProperty(
+                    formFile.Name.Substring(formFile.Name.IndexOf(".",
+                    StringComparison.Ordinal) + 1));
+
+            if (property != null)
+            {
+                if (property.GetCustomAttribute(typeof(DisplayAttribute)) is
+                    DisplayAttribute displayAttribute)
+                {
+                    fieldDisplayName = $"{displayAttribute.Name} ";
+                }
+            }
+
+            var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                formFile.FileName);
+
+            if (formFile.Length == 0)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"Ошибка загруки файла, в нем нет данных.");
+
+                return Array.Empty<byte>();
+            }
+
+            if (formFile.Length > sizeLimit)
+            {
+                var megabyteSizeLimit = sizeLimit / 1048576;
+                modelState.AddModelError(formFile.Name,
+                    $"Превышение допустимого размера файла. Файл не должен превышать " +
+                    $"{megabyteSizeLimit:N1} MB.");
+
+                return Array.Empty<byte>();
+            }
+
+            try
+            {
+                using var memoryStream = new MemoryStream();
+                await formFile.CopyToAsync(memoryStream);
+
+                if (memoryStream.Length == 0)
+                {
+                    modelState.AddModelError(formFile.Name,
+                        $"{fieldDisplayName} не содержит данных.");
+                }
+
+                if (!IsValidFileExtensionAndSignature(
+                    formFile.FileName, memoryStream, permittedExtensions))
+                {
+                    modelState.AddModelError(formFile.Name,
+                        $"{fieldDisplayName}" +
+                        "загрузка фалов с данным типом расширения запрещена или сигнатура файла не соответствует расширению.");
+                }
+                else
+                {
+                    return memoryStream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"{fieldDisplayName} ошибка загрузки." +
+                    $"Пожалуйста обратитесь к администратору: {ex.HResult}");
+            }
+
+            return Array.Empty<byte>();
+        }
+
+        private static bool IsValidFileExtensionAndSignature(string fileName, Stream data, string[] permittedExtensions)
+        {
+            if (string.IsNullOrEmpty(fileName) || data == null || data.Length == 0)
+            {
+                return false;
+            }
+
+            var ext = Path.GetExtension(fileName).ToLowerInvariant();
+
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+            {
+                return false;
+            }
+
+            data.Position = 0;
+
+            using (var reader = new BinaryReader(data))
+            {
+                if (ext.Equals(".txt") || ext.Equals(".csv") || ext.Equals(".prn"))
+                {
+                    if (_allowedChars.Length == 0)
+                    {
+                        for (var i = 0; i < data.Length; i++)
+                        {
+                            if (reader.ReadByte() > sbyte.MaxValue)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                        for (var i = 0; i < data.Length; i++)
+                        {
+                            var b = reader.ReadByte();
+                            if (b > sbyte.MaxValue ||
+                                !_allowedChars.Contains(b))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+
+                    return true;
+                }
+
+                var signatures = _fileSignature[ext];
+                var headerBytes = reader.ReadBytes(signatures.Max(m => m.Length));
+
+                return signatures.Any(signature =>
+                    headerBytes.Take(signature.Length).SequenceEqual(signature));
+            }
+        }
+
+        public static async Task<IFormFile> ChangeFormFile(IFormFile formFile,
+       ModelStateDictionary modelState, string[] permittedExtensions,
+       long sizeLimit)
+        {
+            var fieldDisplayName = string.Empty;
+
+            MemberInfo property =
+                typeof(IFormFile).GetProperty(
+                    formFile.Name.Substring(formFile.Name.IndexOf(".",
+                    StringComparison.Ordinal) + 1));
+
+            if (property != null)
+            {
+                if (property.GetCustomAttribute(typeof(DisplayAttribute)) is
+                    DisplayAttribute displayAttribute)
+                {
+                    fieldDisplayName = $"{displayAttribute.Name} ";
+                }
+            }
+
+            var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                formFile.FileName);
+
+            if (formFile.Length == 0)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"Ошибка загруки файла, в файле отсутствут данных.");
+
+                return formFile;
+            }
+
+            if (formFile.Length > sizeLimit)
+            {
+                var megabyteSizeLimit = sizeLimit / 1048576;
+                modelState.AddModelError(formFile.Name,
+                    $"Превышение допустимого размера файла. Файл не должен превышать " +
+                    $"{megabyteSizeLimit:N1} MB.");
+
+                return formFile;
+            }
+
+            try
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await formFile.CopyToAsync(memoryStream);
+
+                    if (memoryStream.Length == 0)
+                    {
+                        modelState.AddModelError(formFile.Name,
+                            $"Файл {fieldDisplayName} не содержит данных.");
+                    }
+
+                    if (!IsValidFileExtensionAndSignature(
+                        formFile.FileName, memoryStream, permittedExtensions))
+                    {
+                        modelState.AddModelError(formFile.Name,
+                            "Загрузка файлов с данным типом расширения запрещена или сигнатура файла не соответствует расширению.");
+                    }
+                    else
+                    {
+                        return formFile;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"Ошибка загрузки файла {fieldDisplayName}. " +
+                    $"Пожалуйста обратитесь к администратору: {ex.HResult}");
+            }
+
+            return formFile;
+        }
+    }
+}
